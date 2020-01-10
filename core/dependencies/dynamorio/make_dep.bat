2cefdf09922b781f9fc1d31d5f06dff7fd2f7765
Thomas Riedmaier <thomas-josef.riedmaier@siemens.com>
2020-01-10 09:39:35 +0100

    docs: updating file ownership

--- core/dependencies/dynamorio/make_dep.bat
+++ core/dependencies/dynamorio/make_dep.bat
@@ -25,7 +25,7 @@
 :: OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 :: DAMAGE.
 :: 
-:: Author(s): Thomas Riedmaier, Pascal Eckmann
+:: Author(s): Thomas Riedmaier, Pascal Eckmann, Roman Bendt
 
 IF NOT DEFINED VCVARSALL (
 		ECHO Environment Variable VCVARSALL needs to be set!
