2f3d36ffd90f47b959ddc71b604eeaaa14a15dc0
Thomas Riedmaier <thomas-josef.riedmaier@siemens.com>
2020-04-20 10:16:43 +0200

    feat(helper): kFuzzStarter now disables logging in WinDBG (which causes memory exhaustion)

--- core/helpers/Starter/kFuzzStarter/kFuzzStarter.cpp
+++ core/helpers/Starter/kFuzzStarter/kFuzzStarter.cpp
@@ -12,6 +12,9 @@ Author(s): Abian Blome, Thomas Riedmaier
 
 #include "stdafx.h"
 
+std::string loggingFuncBytes("\x73\x7D\x02\x00\x06\x0A\x06\x02\x7D\xBA\x01\x00\x04\x06\x03\x7D\xBB\x01\x00\x04\x06\x06\xFE\x06\x7E\x02\x00\x06\x73\x19\x01\x00\x0A\x7D\xBC\x01\x00\x04\x02\x7B\xAC\x00\x00\x04\x28\x2C\x01\x00\x0A\x6F\x29\x01\x00\x0A\x2E\x19\x02\x7B\xAB\x00\x00\x04\x06\xFE\x06\x7F\x02\x00\x06\x73\x2D\x01\x00\x0A\x14\x6F\x2E\x01\x00\x0A\x2A\x06\x7B\xBC\x01\x00\x04\x6F\x2F\x01\x00\x0A\x2A", 0x5d);
+std::string loggingFuncDisabledBytes("\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x2A", 0x5d);
+
 void execCommand(std::string command) {
 	printf("executing command %s\n", command.c_str());
 
@@ -117,6 +120,106 @@ int startvm(
 
 	return 0;
 }
+int  search(std::istream_iterator<char>  first, std::istream_iterator<char>  last, std::string::iterator  s_first, std::string::iterator  s_last)
+{
+	int current_position = -1;
+
+	for (; ; ++first) {
+		current_position++;
+		std::istream_iterator<char>  it = first;
+
+		for (std::string::iterator s_it = s_first; ; ++it, ++s_it) {
+			if (s_it == s_last) {
+				return current_position - std::distance(s_first, s_last);
+			}
+			if (it == last) {
+				return -1;
+			}
+			if (!(*it == *s_it)) {
+				break;
+			}
+			current_position++;
+		}
+	}
+}
+
+bool doesFileContainPattern(std::experimental::filesystem::path filename, std::string pattern) {
+	//Get an input stream for the filename
+	std::ifstream ifs(filename.c_str(), std::ios_base::binary);
+
+	if (!ifs) {
+		std::cout << "failed to open file";
+		return false;
+	}
+
+	ifs.unsetf(std::ios::skipws);
+
+	// begin = start of file
+	// end = end of file
+	std::istream_iterator<char> begin(ifs), end;
+
+	// search file for string
+	if (search(begin, end, pattern.begin(), pattern.end()) != -1)
+	{
+		//The pattern is present
+		return true;
+	}
+
+	//The pattern is missing
+	return false;
+}
+
+bool isWindbgLoggingDisabled(std::string pathToWinDBGPrev) {
+	std::experimental::filesystem::path windbgExePath(pathToWinDBGPrev);
+
+	return doesFileContainPattern(windbgExePath.parent_path().append("DbgX.Services.dll"), loggingFuncDisabledBytes);
+}
+
+bool disableWindbgLogging(std::string pathToWinDBGPrev) {
+	std::experimental::filesystem::path windbgExePath(pathToWinDBGPrev);
+
+	//Check if the file can be patched
+	bool canWePatchThisFile = doesFileContainPattern(windbgExePath.parent_path().append("DbgX.Services.dll"), loggingFuncBytes);
+	if (!canWePatchThisFile) {
+		std::cout << "We cannot disable the logging, as the DbgX.Services.dll does not contain the expected pattern." << std::endl;
+		return false;
+	}
+
+	//Create a backup
+	try
+	{
+		std::experimental::filesystem::copy(windbgExePath.parent_path().append("DbgX.Services.dll"), windbgExePath.parent_path().append("DbgX.Services.dll.orig"), std::experimental::filesystem::copy_options::skip_existing);
+	}
+	catch (...) {
+		std::cout << "We cannot disable the logging, as the WinDBG directory does not allow creating files." << std::endl;
+		return false;
+	}
+
+	//Patch the DbgX.Services.dll
+	{
+		std::fstream fs(windbgExePath.parent_path().append("DbgX.Services.dll").c_str(), std::fstream::in | std::fstream::out | std::fstream::binary);
+		if (!fs) {
+			std::cout << "We cannot disable the logging, as the DbgX.Services.dll could not be opened for writing." << std::endl;
+			return false;
+		}
+		fs.unsetf(std::ios::skipws);
+		std::istream_iterator<char> begin(fs), end;
+		std::iostream::pos_type position_of_data_to_overwrite = search(begin, end, loggingFuncBytes.begin(), loggingFuncBytes.end()); //We already checked that the pattern is there
+		fs.seekp(position_of_data_to_overwrite, std::ios_base::beg);
+		fs.write(loggingFuncDisabledBytes.c_str(), loggingFuncDisabledBytes.size());
+		fs.flush();
+		fs.close();
+	}
+
+	//Check if we succeeded
+	bool didPatchSucceed = !doesFileContainPattern(windbgExePath.parent_path().append("DbgX.Services.dll"), loggingFuncBytes) && doesFileContainPattern(windbgExePath.parent_path().append("DbgX.Services.dll"), loggingFuncDisabledBytes);
+	if (!canWePatchThisFile) {
+		std::cout << "We cannot disable the logging as patching the DbgX.Services.dll failed" << std::endl;
+		return false;
+	}
+
+	return true;
+}
 
 int main(int argc, char* argv[])
 {
@@ -135,7 +238,6 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	bool startVM = false;
 	std::string nameOfVM = "";
 	std::string pathToLocalExecutableAndArgs = "";
 	int sleepMS = 0;
@@ -151,32 +253,26 @@ int main(int argc, char* argv[])
 
 	for (int i = 1 /*skip program name*/; i < argc - 1; i++) {
 		if (std::string(argv[i]) == "--pipeName") {
-			startVM = true;
 			i++;
 			pipeName = std::string(argv[i]);
 		}
 		if (std::string(argv[i]) == "--startVM") {
-			startVM = true;
 			i++;
 			nameOfVM = std::string(argv[i]);
 		}
 		if (std::string(argv[i]) == "--kfuzzWindbg") {
-			startVM = true;
 			i++;
 			kfuzzWindbg = std::string(argv[i]);
 		}
 		if (std::string(argv[i]) == "--vmwarePath") {
-			startVM = true;
 			i++;
 			pathToVMWare = std::string(argv[i]);
 		}
 		if (std::string(argv[i]) == "--vmmonPath") {
-			startVM = true;
 			i++;
 			vmmonPath = std::string(argv[i]);
 		}
 		if (std::string(argv[i]) == "--winDbgPrevPath") {
-			startVM = true;
 			i++;
 			winDbgPrevPath = std::string(argv[i]);
 		}
@@ -185,7 +281,6 @@ int main(int argc, char* argv[])
 			pathToLocalExecutableAndArgs = std::string(argv[i]);
 		}
 		if (std::string(argv[i]) == "--sleep") {
-			startVM = true;
 			i++;
 			sleepMS = std::stoi(std::string(argv[i]));
 		}
@@ -200,9 +295,17 @@ int main(int argc, char* argv[])
 		}
 	}
 
-	if (startVM) {
-		startvm(nameOfVM, pathToVMWare, additionalCommands, restorecurrent, snapshotName, vmmonPath, winDbgPrevPath, pipeName, kfuzzWindbg, gdbInitFile, sleepMS);
+	if (!isWindbgLoggingDisabled(winDbgPrevPath)) {
+		std::cout << "WinDBG logging is not yet disabled. We need to do so in order to prevent memory resource exhaustion." << std::endl;
+		if (!disableWindbgLogging(winDbgPrevPath)) {
+			std::cout << "Failed to disable WinDBG logging." << std::endl;
+			return -1;
+		}
+		std::cout << "Successfully disabled WinDBG logging." << std::endl;
 	}
+
+	startvm(nameOfVM, pathToVMWare, additionalCommands, restorecurrent, snapshotName, vmmonPath, winDbgPrevPath, pipeName, kfuzzWindbg, gdbInitFile, sleepMS);
+
 	if (!pathToLocalExecutableAndArgs.empty()) {
 		execCommandAsync(pathToLocalExecutableAndArgs);
 	}
